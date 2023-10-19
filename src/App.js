4860bf962b8198b8442ef257500c9486719092fc
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2023-10-19 22:27:15 +0545

    feat: page background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Blog, Footer, Features, Possibility, Header, WhatGPT3} from './containers';
 import {Article, Brand, CTA, Navbar} from './components';
+import './App.css'
 
 const App = () => {
   return (
