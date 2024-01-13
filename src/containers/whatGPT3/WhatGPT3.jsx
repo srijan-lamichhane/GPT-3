9658b04dbdd99d9ea6e24efa1ccae54ccbc8bcfe
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-01-13 19:09:45 +0545

    feat:whatgpt3_beginning

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,11 +1,26 @@
-import './whatgpt3.css'
-
 import React from 'react'
+import './whatgpt3.css'
+import Feature from '../features/Features' 
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      WhatGPT3
+    <div className="gpt3__whatgpt3 section__padding" id="whatgpt3">
+
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature /> 
+      </div>
+
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    
     </div>
   )
 }
