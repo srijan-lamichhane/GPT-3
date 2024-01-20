202b16b4173aa72f823b6aed489e2be928ad44b6
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-01-20 20:02:18 +0545

    fix: padding margin problem fixed in header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,7 +6,7 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header section_padding" id="home">
       <div className="gpt3__header-content">
 
         <h1 className="gradient__text">
