5e99b3e674847e65062930e154e63f8d2508c0a7
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-01-26 13:24:48 +0545

    feat: cta section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -4,8 +4,16 @@ import React from 'react'
 
 const CTA = () => {
   return (
-    <div>
-      CTA
+    <div className='gpt3__cta'>
+
+      <div className="gpt3__cta-content">
+      <p>Request Early Access to Get Started</p>
+      <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type='button'>Get started</button>  
+      </div>  
+      
     </div>
   )
 }
