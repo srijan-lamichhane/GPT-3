db78b3b3f50085bbff3b9e84490a784fa1433fd0
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-01-30 18:08:29 +0545

    feat: blog full responsiveness

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -7,7 +7,7 @@ import React from 'react'
 const Blog = () => {
   return (
     <div className='gpt3__blog section_padding'>
-      <div className='gpt3__blog-header'>
+      <div className='gpt3__blog-heading'>
         <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
       </div>
       <div className="gpt3__blog-container">
